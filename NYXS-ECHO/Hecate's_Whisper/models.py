146ce9ebdee53b597cae245b037ced3238c08a7e
ianmongi45 <ianmongi45@gmail.com>
2024-08-25 15:37:32 +0000

    first commit

---
+++ NYXS-ECHO/Hecate's_Whisper/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=30, blank=True)
---
+++ NYXS-ECHO/Hecate's_Whisper/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect 
+from django.contrib.auth import login, authenticate, logout 
+from .forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        elif User is None:
+            error_message = 'User does not exist'
+            return render(request, 'signup.html', {'error_message': error_message})
+        else:
+            error_message = 'Invalid username or password'
+            return render(request, 'login.html', {'error_message': error_message})
+    return render(request, 'login.html')
+
